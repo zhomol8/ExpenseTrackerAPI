c5124aa7599eff0ca0a9a66afa2d8fdc58db1745
zhomol <zhomol8@gmail.com>
2025-06-30 19:30:08 -0400

    First commit

--- obj/Debug/net9.0/ExpenseTrackerAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExpenseTrackerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033143b461b8b1ef5a9b177408d086718e5cd666")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
